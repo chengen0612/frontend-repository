c20234b3c2639baf267b2ed08a7a99fc5a3c32e1
evans <chengen0612@gmail.com>
2024-07-03 16:47:52 +0800

    feat: add post page

---
+++ app/(route)/post/[id]/page.js
@@ -0,0 +1,48 @@
+import { client } from "@/app/_client";
+import Comment from "./components/comment";
+import { Suspense } from "react";
+
+export default async function Page({ params }) {
+  const { id } = params;
+  const post = await client.post.retrieve(id);
+  const comments = await client.post.comments(id);
+  const user = await client.user.retrieve(post.userId);
+  const avatar = await client.user.avatar(user.username);
+
+  return (
+    <div className="mt-9 mx-auto w-1/2">
+      <h2 className="text-4xl font-semibold">{post.title}</h2>
+
+      <div className="flex items-center gap-x-6 mt-9">
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img src={avatar.medium} alt={user.username} className="rounded-full" />
+
+        <div>
+          <div className="font-bold text-cyan-700">
+            {user.email.split("@")[0]}
+          </div>
+          <div>{post.body}</div>
+        </div>
+      </div>
+
+      <div className="mt-9 flex justify-center gap-x-3">
+        {[...Array(5)].map((_, i) => (
+          <span
+            key={i}
+            className="aspect-square rounded-full w-3 bg-gray-200 inline-block"
+          ></span>
+        ))}
+      </div>
+
+      <ul className="mt-9">
+        {comments.map((c) => (
+          <li key={c.id} className="[&:not(:first-child)]:mt-6">
+            <Suspense fallback="Loading...">
+              <Comment comment={c} />
+            </Suspense>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
